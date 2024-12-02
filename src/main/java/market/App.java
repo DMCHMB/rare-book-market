30ebc2d0138fe48bb21b391b2c5df2918f971ed4
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2024-12-02 13:34:48 +0300

    fix encodings

--- src/main/java/market/App.java
+++ src/main/java/market/App.java
@@ -2,6 +2,6 @@ package src.main.java.market;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 4 дня");
+        System.out.println("РћС‚РєСЂС‹С‚РёРµ РјР°РіР°Р·РёРЅР° РѕР¶РёРґР°РµС‚СЃСЏ С‡РµСЂРµР· 4 РґРЅСЏ");
     }
 }
